f60fdc09f6075371d31dec67c5b50f3dff41f231
udaykumar <udaykumarsamidala57@gmail.com>
2026-02-24 18:19:03 +0530

    Your commit message

---
+++ src/main/java/bean/DBUtil.java
@@ -0,0 +1,31 @@
+package bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+
+	
+	private static final String URL = "jdbc:mysql://shuttle.proxy.rlwy.net:26985/HR";
+    private static final String USER = "root";
+	private static final String PASSWORD = "vSZVibKCzvcovcGjaLlxrTddrjiNPVQn";
+	
+	
+	
+	
+	
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+        	
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
